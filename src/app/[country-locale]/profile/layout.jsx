fb44b00f2443143b6f9d01353bf48cfca675b876
Mahmoud <mahmoudaddas393@gmail.com>
2025-07-03 17:05:42 +0300

    feat: add MobileHeaderComponent for mobile profile header

--- src/app/[country-locale]/profile/layout.jsx
+++ src/app/[country-locale]/profile/layout.jsx
@@ -1,12 +1,15 @@
+import MobileHeaderComponent from "@/components/profile/MobileHeaderComponent";
 import ProfileTabs from "@/components/profile/ProfileTabs";
 import React from "react";
-
 
 const ProfileLayout = ({ children }) => {
   return (
     <div className="profile-page">
       <div className="container ">
         <div className="row m-0">
+          <div className="d-md-none ">
+            <MobileHeaderComponent />
+          </div>
           <div className="col-12  col-md-4 col-lg-3  p-2">
             <ProfileTabs />
           </div>
