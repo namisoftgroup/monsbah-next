cf71ab4953f42a0931d2fd546bbcce1abbc6342a
Mahmoud <mahmoudaddas393@gmail.com>
2025-08-11 10:19:12 +0300

    feat : iso code fix

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -1,20 +1,20 @@
 import { defineRouting } from "next-intl/routing";
 
 export const LOCALES = [
-  "saudia-ar",
-  "saudia-en",
-  "kuwait-ar",
-  "kuwait-en",
-  "emirates-ar",
-  "emirates-en",
-  "bahrain-ar",
-  "bahrain-en",
-  "oman-ar",
-  "oman-en",
-  "qatar-ar",
-  "qatar-en",
+  "sa-ar",
+  "sa-en",
+  "kw-ar",
+  "kw-en",
+  "ae-ar",
+  "ae-en",
+  "bh-ar",
+  "bh-en",
+  "om-ar",
+  "om-en",
+  "qa-ar",
+  "qa-en",
 ];
-export const DEFAULT_LOCALE = "saudia-ar";
+export const DEFAULT_LOCALE = "sa-ar";
 
 export const routing = defineRouting({
   locales: LOCALES,
