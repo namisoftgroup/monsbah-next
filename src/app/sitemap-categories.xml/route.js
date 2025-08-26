f16cc0ee3be3a72dfad33d8084f11b2181570f5d
Mahmoud Elkomy <mahmoudelkomy996@gmail.com>
2025-08-26 13:35:59 +0300

    refactor(sitemap): move sitemap routes to root app directory
    Restructure sitemap routes by moving them from [country-locale] to the root app directory to simplify routing and improve maintainability. This change also includes updating the sitemap-index.xml route with additional functionality.

--- src/app/sitemap-categories.xml/route.js
+++ src/app/sitemap-categories.xml/route.js
@@ -37,8 +37,8 @@ export async function GET() {
     });
 
     // Generate XML sitemap
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    const xml = `<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">
 ${sitemapEntries
   .map(
     (entry) => `  <url>
